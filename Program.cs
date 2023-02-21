ce3f87fc73e6b6941ae03af94f72be7b0dd0c111
Hakan ÖZCAN <hozcan@tumsas.local>
2023-02-21 10:00:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+//System.Diagnostics kütüphanesinde bulunan “Stopwatch” sınıfıyla kodlarımızın çalışma sürelerini hızlı bir şekilde bulup, istediğimiz formatta değerlendirebiliyoruz.
+
+Stopwatch SureHesapla = new Stopwatch();
+
+SureHesapla.Start();
+
+Thread.Sleep(9000);
+
+SureHesapla.Stop();
+
+TimeSpan HesaplananZaman = SureHesapla.Elapsed;
+string Sonuc = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+HesaplananZaman.Hours, HesaplananZaman.Minutes, HesaplananZaman.Seconds, HesaplananZaman.Milliseconds / 10);
+Console.WriteLine(Sonuc);
